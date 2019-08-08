a47cda6c0e25c0d6353a8666c3a0eac6ee3ab5a5
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-08 15:40:22 -0400

    dashboard set up

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-import Display from './components/Display';
+// import Display from './components/Display';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>TESTING</h1>
-      <Display />
+      {/* <Display /> */}
       <Dashboard />
     </div>
   );
