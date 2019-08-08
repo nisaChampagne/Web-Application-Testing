fb2030506dd3a608c60542c0b9d91870768f5949
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-08 15:29:29 -0400

    updated  to delete unnecessary stuff

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>TESTING</h1>
+      <Dashboard />
     </div>
   );
 }
